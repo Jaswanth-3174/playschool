636f0b05fe4559cd1192b949a66b6eb410891003
Jaswanth <s.jaswanth2004@gmail.com>
2025-06-12 22:15:57 +0530

    changed

--- client/src/components/sections/contact.tsx
+++ client/src/components/sections/contact.tsx
@@ -55,10 +55,11 @@ export default function Contact() {
   ];
 
   const socialLinks = [
-    { icon: "fab fa-facebook", color: "coral", href: "#" },
-    { icon: "fab fa-instagram", color: "turquoise", href: "#" },
-    { icon: "fab fa-twitter", color: "sky", href: "#" },
-    { icon: "fab fa-youtube", color: "mint", href: "#" }
+    { icon: "fab fa-facebook", color: "coral", href: "https://www.facebook.com/share/16XNhfiZaU/" },
+    { icon: "fab fa-instagram", color: "turquoise", href: "https://www.instagram.com/robudz_playschool?igsh=MTlwdHU4eTdwcWlneA==" },
+    // { icon: "fab fa-twitter", color: "sky", href: "#" },
+    { icon: "fab fa-youtube", href: "https://www.youtube.com/@robudzplayschool?si=fpkW5QqtubU3v8Qh" }
+    { icon: "fab fa-youtube", color: "mint", href: "" }
   ];
 
   const handleInputChange = (field: string, value: string) => {
